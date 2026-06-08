733c1ce1ccc1e0882d2ac1cf3be811edb606c082
ATDevs-Team <acc@atdevs.org>
2026-06-07 19:54:48 -0500

    Add license information to nucleares_state.py

--- nucleares_state.py
+++ nucleares_state.py
@@ -1,3 +1,20 @@
+'''
+    Nucleares Mod Tool Save Parser/Modder Library
+    Copyright (C) 2026  ATDevs Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 import xml.etree.ElementTree as ET
 import copy
 from nucleares_io import decode_payload, encode_payload
