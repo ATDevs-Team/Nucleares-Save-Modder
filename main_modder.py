f490110d6fc27d809f385eb76850d67cd7d4e69e
ATDevs-Team <acc@atdevs.org>
2026-06-07 19:53:25 -0500

    Rename application title in main_modder.py

--- main_modder.py
+++ main_modder.py
@@ -1,3 +1,20 @@
+'''
+    Nucleares Mod Tool
+    Copyright (C) 2026  ATDevs Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, scrolledtext
 import xml.etree.ElementTree as ET
@@ -9,7 +26,7 @@ from nucleares_state import SaveMemoryManager
 class ModderApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Nucleares Ultimate Modder (Multi-Module)")
+        self.root.title("Nucleares Save Modder (Multi-Module)")
         self.root.geometry("800x680")
         
         self.memory = None
